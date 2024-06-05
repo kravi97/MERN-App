372d60d8dac1db8b795fddf58dbcc54a522cd692
Ravindra Kawale <ravindra.kawale@waiin.com>
2024-06-05 12:14:21 +0530

    video 53 completed.

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -50,4 +50,44 @@ router.post('/createuser', [
         }
     });
 
+//Authenticate a user using : POST "/api/auth/login". Doesn't require Auth
+router.post('/login', [
+    body('email', 'Enter a valid email').isEmail(),
+    body('password', `Password can't be blank `).exists()],
+    async (req, res) => {
+        //if there are errors, return Bad request and the errors
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ error: "Please try to login with correct credentials" });
+            }
+
+            const passwordCompare = await bcrypt.compare(password, user.password);
+            if (!passwordCompare) {
+                return res.status(400).json({ error: "Please try to login with correct credentials" });
+            }
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const authToken = jwt.sign(data, JWT_SECRET);
+            res.json({ authToken });
+
+        } catch (error) {
+            console.log(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    });
+
+
 module.exports = router;
