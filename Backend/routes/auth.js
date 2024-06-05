3e0d5f0687b56f962e94acc44546d4f71655700a
Ravindra Kawale <ravindra.kawale@waiin.com>
2024-06-05 10:01:40 +0530

    video 52 completed.

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'Harryisagood$boy';
 
 //Creating a new user using : POST "/api/auth/". Doesn't require Auth
 router.post('/createuser', [
@@ -20,12 +24,26 @@ router.post('/createuser', [
             if (user) {
                 return res.status(400).json({ error: "Sorry a user with this email already exists" });
             }
+
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+
+            //Create a new user
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
-            })
-            res.json({ 'user': user });
+                password: secPass
+            });
+
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            const authToken = jwt.sign(data, JWT_SECRET);
+
+            res.json({ authToken });
         } catch (error) {
             console.log(error.message);
             res.status(500).send("Some error occured");
