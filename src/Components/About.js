6371095e2a1a5f6c81859e7a8161c523f6928436
Ravindra Kawale <ravindra.kawale@waiin.com>
2024-06-07 08:42:22 +0530

    video 59 completed.

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,17 +1,9 @@
-import React, { useContext, useEffect } from 'react'
-import NoteContext from '../context/notes/noteContext';
+import React from 'react'
 
 function About() {
-    const a = useContext(NoteContext);
-
-    useEffect(() => {
-        a.update();
-        // eslint-disable-next-line
-    }, [])
-
     return (
         <div>
-            <h1>About {a.state.name} and he is in {a.state.class} class</h1>
+            <h1>About </h1>
         </div>
     )
 }
